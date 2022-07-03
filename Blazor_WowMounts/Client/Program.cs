f3ac7dca9199a23fac51d93dbca06c8b8fc68ca6
Cory Fabian <Coryf65@users.noreply.github.com>
2022-07-03 11:42:51 -0500

    Add project files.

---
+++ Blazor_WowMounts/Client/Program.cs
@@ -0,0 +1,18 @@
+using Blazor_WowMounts.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("Blazor_WowMounts.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Blazor_WowMounts.ServerAPI"));
+
+builder.Services.AddApiAuthorization();
+
+await builder.Build().RunAsync();
---
+++ Blazor_WowMounts/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Blazor_WowMounts.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Blazor_WowMounts.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
